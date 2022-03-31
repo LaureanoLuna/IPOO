34ba1c1644b6bdc8fecc4382d66edcbb4d18a3a9
Laureano Luna <93739755+LaureanoLuna@users.noreply.github.com>
2022-03-30 21:06:56 -0300

    Add files via upload

---
+++ Intro_POO 2022/segundoTp/Test_CuentaBancaria.php
@@ -0,0 +1,17 @@
+<?php
+
+include('CuentaBancaria.php');
+include('../segundoTp/Persona.php');
+$n=["nombre"=>"Laureano","apellido"=>"Luna"];
+$d=["tipoDNI"=>"DNI","numeroDNI"=>38232325];
+
+$objPersona = new Persona($n, $d);
+
+$objCuenta = new CuentaBancaria(3543,$objPersona,50000,20);
+
+//echo $objCuenta;
+$objCuenta->ActualizarSaldo();
+$objCuenta->Depositar(15000);
+//echo $objCuenta;
+$objCuenta->Retirar(65000);
+?>
